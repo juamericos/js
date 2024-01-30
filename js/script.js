64c75a2ac096e0ce93a7196e99e7d7cb91c571f6
Júlia Américo <155107570+juamericos@users.noreply.github.com>
2024-01-30 17:53:33 -0300

    ATUALIZADO!

---
+++ js/script.js
@@ -0,0 +1,40 @@
+/* ATIVIDADE 1
+
+//function tabuada() {
+//    let tab = +prompt("Digite um número!");
+//    for (let index = 1; index <= 10; index++) {
+//        console.log(`${tab} x ${index} = ${tab * index}`);
+//    }
+//}
+*/
+
+
+/* ATIVIDADE 2
+
+function fatorial() {
+
+    let numb = prompt("Digite um número para calcularmos o fatorial!");
+
+    if (numb === 0 || numb === 1) {
+
+        console.log("O fatorial de " + numb + " é 1");
+    } else {
+
+        let fatorial = 1;
+        for (let index = numb; index > 1; index--) {
+
+            fatorial *= index;
+        }
+
+        console.log("O fatorial de " + numb + " é " + fatorial);
+    }
+}
+*/ 
+
+
+/* ATIVIDADE 3
+
+function adivinhar() {
+
+}
+*/
